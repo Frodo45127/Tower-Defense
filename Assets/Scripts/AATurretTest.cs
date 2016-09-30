026fc7f47ae104b1138cf56c47b12ec9c35d1c85
yodefuensa <jaime993@gmail.com>
2016-09-30 15:15:11 +0200

    Actualización menor
    Hay un nuevo prefab de torreta, con un script basicamente casi identico
    al de la torres que ya hay, pero que creo que en teroia debería disparar
    a objetivos aereos, he puesto los sprites de 2 monstruos más, lo proximo
    que haga sera hacer un monstruo con el pajaro y ver si funciona

---
+++ Assets/Scripts/AATurretTest.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//-----------------------------------------------------------------------
+// TurretTest2.cs
+// 
+// Este script es el que se encarga del comportamiento de la segunda
+// torreta de pruebas.
+//
+// NOTA: las variables que se heredan de Turret (coste, rango, daño,...)
+// se ponen en el inspector (unity), no aquí.
+//
+//-----------------------------------------------------------------------
+
+public class TurretTest2 : Turret {
+
+	// la bala a disparar
+	public GameObject bullet;
+
+	// tiempo entre disparos
+	private float timerShot = 1f;
+	private float timerShotReset = 1f;
+
+	void FixedUpdate() {
+
+		// si tenemos enemigo
+		if (targetEnemy != null) {
+
+			// y es volador
+			if (targetEnemy.GetComponent<Enemy>().isFlying) {
+				
+				// haz un temporizador
+				timerShot -= Time.deltaTime;
+				if (timerShot <= 0) {
+
+					// y dispara la bala con la rotacion que tenga la torreta desde el cañon
+					GameObject shotedBullet = (GameObject)Instantiate (bullet, new Vector2 (turretTop.position.x, turretTop.position.y + 0.6f), turretTop.rotation);
+
+					// y le decimos a la bala el daño que debe tener
+					shotedBullet.GetComponent<CannonBall> ().damage = damage;
+
+					// y le decimos a la bala la posición a la que debe dirigirse
+					shotedBullet.GetComponent<CannonBall> ().targetTransform = targetEnemy.transform;
+
+					// y reseteamos el temporizador
+					timerShot = timerShotReset;
+				}	
+			}
+		}
+	}
+}
