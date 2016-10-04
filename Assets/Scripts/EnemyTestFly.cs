ef1d34e33375c4f8f482a2d7b21debf0c8c202e5
yodefuensa <jaime993@gmail.com>
2016-10-04 22:43:58 +0200

    Añadido nuevo enemigo volador
    faltaria meterle en el juego para que aparezca, ya me explicaras como se
    hace. Tiene script propio, animación y colisiones

---
+++ Assets/Scripts/EnemyTestFly.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//-----------------------------------------------------------------------
+// EnemyTest.cs
+// 
+// Este script es el que define el comportamiento del bicho de pruebas.
+// Por ahora el comportamiento es comun para todos, aqui solo va la
+// funcion Start().
+//
+//-----------------------------------------------------------------------
+
+public class EnemyTestFly : Enemy {
+
+	// Use this for initialization
+	void Start () {
+
+		// cacheo del transform
+		myTransform = transform;
+
+		// vida y velocidad del bicho
+		healthPoints = 3;
+		enemySpeed = 100f;
+		score = 40;
+		money = 20;
+		damage = 1;
+		isFlying = true;
+
+		// seteamos el máximo de vida y la vida inicial
+		healthBar.maxValue = healthPoints;
+		healthBar.value = healthPoints;
+
+		// hacemos invisible el canvas de la barra de vida mientras la vida esté a tope
+		enemyUI.enabled = false;
+
+		// cogemos la lista de nodos (camino) que haya en el spawner
+		pathToFollow = spawner.Path;
+
+		// ponemos como actual el primer nodo de la lista
+		currentNodeInTheList = 0;
+		currentNode = pathToFollow [currentNodeInTheList];
+	}
+}
